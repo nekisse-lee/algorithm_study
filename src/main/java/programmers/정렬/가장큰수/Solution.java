7c3bb91e9e489e5d8d024a9eadde488e2c8ff390
nekisse <lsh891224@gmail.com>
2019-09-16 16:56:17 +0900

    프로그래머스 - 정렬_가장큰수

--- src/main/java/programmers/정렬/가장큰수/Solution.java
+++ src/main/java/programmers/정렬/가장큰수/Solution.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-//        int[] arr = {3, 30, 34, 5, 9};
+//        int[] arr = {3, 30, 34, 5, 9};;
         int[] arr = {0};
 
         System.out.println(solution(arr));
